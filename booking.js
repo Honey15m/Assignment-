bac2aafba4db976edb88524196daabea776780ff
Honey15m <111412243+Honey15m@users.noreply.github.com>
2022-08-25 02:00:21 -0700

    Delete booking.js

--- booking.js
+++
@@ -1,9 +0,0 @@
-var form = document.getElementById("formAwesome");
-form.addEventListener("submit", onSubmitForm);
-
-function onSubmitForm(e) {
-  e.preventDefault();
-  $('#formModal').modal('hide');
-  $('#btnStart').hide();
-  $('#message').show();
-}
